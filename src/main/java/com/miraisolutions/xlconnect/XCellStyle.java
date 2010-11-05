92e4dc9fa16c1eeb47c255288d9d7561138af9b1
mirai <mirai@ccc6e944-403e-a54a-b5f3-c2babcd103ea>
2010-11-05 14:10:02 +0000

    MS: including NOTICE for attribution to Apache (according to license terms); including GPLv3 header in every source file
    git-svn-id: svn://ms72.weblink.ch/mirai/XLConnect@45 ccc6e944-403e-a54a-b5f3-c2babcd103ea

--- src/main/java/com/miraisolutions/xlconnect/XCellStyle.java
+++ src/main/java/com/miraisolutions/xlconnect/XCellStyle.java
@@ -1,6 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *
+    XLConnect
+    Copyright (C) 2010 Mirai Solutions GmbH
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package com.miraisolutions.xlconnect;
@@ -26,23 +41,16 @@ import org.openxmlformats.schemas.spreadsheetml.x2006.main.STBorderStyle;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.STPatternType;
 
 /**
- * Uses code from XSSFCellStyle from
+ * This class uses parts from XSSFCellStyle.java at
  * http://svn.apache.org/repos/asf/poi/trunk/src/ooxml/java/org/apache/poi/xssf/usermodel/XSSFCellStyle.java
+ * by The Apache Software Foundation
  * 
  * @author Martin Studer, Mirai Solutions GmbH
  */
 public class XCellStyle implements CellStyle {
 
-    /**
-     * getCoreXf().setXXX(...)
-     * getStyleXf().setXXX(...)
-     * getStyleXf().applyXXX(...)
-     */
-
     private final XSSFWorkbook workbook;
-    // private final StylesTable stylesSource;
     private final int xfId, styleXfId;
-    // private final ThemesTable themes;
     private XSSFCellAlignment cellAlignment;
 
     public XCellStyle(XSSFWorkbook workbook, int xfId, int styleXfId) {
